fb696a20cc98db8472352e2feb42638f102b1429
Tamir <door2k@gmail.com>
2014-09-29 14:25:53 +0300

    Initial Commit

---
+++ swiftBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftBeacon
+//
+//  Created by Tamir Berliner on 9/29/14.
+//  Copyright (c) 2014 Tamir Berliner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
